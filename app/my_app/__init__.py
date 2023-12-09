a8a003f04e0ddc19dd6915204f85b3a6f5e56cbd
efuentes <efuentes@lsv-tech.com>
2023-12-09 18:52:11 -0500

    Flask Migration

--- app/my_app/__init__.py
+++ app/my_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from my_app.config import DevConfig
 
@@ -9,13 +10,14 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 db=SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 from my_app.tasks.controller import taskRoute
 
 app.register_blueprint(taskRoute)
 
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 @app.route('/')
 def hello_world():
